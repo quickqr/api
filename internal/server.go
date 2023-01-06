cb1fd0bf250c02252f380051b56c8856f1c51c3e
kwinso <ruslan.sot70@gmail.com>
2023-01-06 23:45:23 +0300

    Add GET variation of /v1/generate

--- internal/server.go
+++ internal/server.go
@@ -2,14 +2,17 @@ package internal
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"gitlab.com/quick-qr/server/internal/handlers"
+	"time"
 )
 
 func RunServer(port string) error {
-	app := fiber.New()
+	app := fiber.New(fiber.Config{StrictRouting: false})
 
-	app.Static("/docs", "./docs/public")
+	app.Use(cors.New())
 
+	app.Static("/docs/", "./docs/public", fiber.Static{CacheDuration: time.Hour})
 	handlers.Register(app)
 
 	return app.Listen(":" + port)
